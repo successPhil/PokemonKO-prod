dd631b06f879d2725f7476006555965e5fcd5f36
successPhil <pbasti1990@gmail.com>
2024-05-08 19:44:57 -0400

    all tests passing locally, pushing before major refactor to backend and frontend. See project issues for details

--- backend/old_poke/settings.py
+++ backend/old_poke/settings.py
@@ -94,11 +94,11 @@ WSGI_APPLICATION = 'old_poke.wsgi.application'
 DATABASES = {
 "default": {
 "ENGINE": "django.db.backends.postgresql",
-"NAME": "poke_db", 
+"NAME": "pokemon_db", 
 "USER": "postgres",
 "PASSWORD": "postgres",
 "HOST": "localhost",
-"PORT": 5432,
+"PORT": 5454,
     }
 }
 
