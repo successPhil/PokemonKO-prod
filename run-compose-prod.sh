ec1fe06e3eda778680ad381db93952ea0c0667a8
successPhil <pbasti1990@gmail.com>
2024-05-17 00:34:45 -0400

    updated prod compose script to use kwarg instead of positional args

--- run-compose-prod.sh
+++ run-compose-prod.sh
@@ -5,12 +5,12 @@ export DOCKERHUB_UNAME=successphil
 
 # These environment variables come from command line arguments.
 # They are consumed by the docker-compose file.
-export SECRET_KEY=$1
-export DEBUG=$2
-export POSTGRES_DB=$3
-export POSTGRES_USER=$4
-export POSTGRES_PASSWORD=$5
-export NEW_VERSION=$6
+export SECRET_KEY=$SECRET_KEY
+export DEBUG=$DEBUG
+export POSTGRES_DB=$POSTGRES_DB
+export POSTGRES_USER=$POSTGRES_USER
+export POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+export NEW_VERSION=$NEW_VERSION
 
 docker-compose -f docker-compose.prod.yml build --no-cache
 docker-compose -f docker-compose.prod.yml up -d
